83835489cf78e8919005e0d492a01ffa00abb69c
Azidara <albertbode@mac.com>
2023-02-09 17:13:10 -0600

    Updated Arrow2D to render both sides, have color picking built in and update when changes are made in the inspector

--- ShapeTools/Arrow2D.cs
+++ ShapeTools/Arrow2D.cs
@@ -10,23 +10,36 @@ public class Arrow2D : MonoBehaviour
     [SerializeField] private float _stemWidth = 0.1f;
     [SerializeField] private float _pointLength = 0.5f;
     [SerializeField] private float _pointWidth = 0.5f;
+    [SerializeField] private Color _color = Color.white;
 
     public float stemLength {get{return _stemLength;} set{_stemLength = value; this.MakeArrowMesh();}}
     public float stemWidth {get{return _stemWidth;} set{_stemWidth = value; this.MakeArrowMesh();}}
     public float pointLength {get{return _pointLength;} set{_pointLength = value; this.MakeArrowMesh();}}
     public float pointWidth {get{return _pointWidth;} set{_pointWidth = value; this.MakeArrowMesh();}}
+    public Color color {get{return _color;} set{_color = value; this.UpdateColor();}}
 
     [System.NonSerialized]
     public List<Vector3> vertices;
     [System.NonSerialized]
     public List<int> triangles;
+
     Mesh mesh;
-    void Start()
-    {
+
+    void Awake(){
         mesh = new Mesh();
         this.GetComponent<MeshFilter>().mesh = mesh;
 
         MakeArrowMesh();
+        UpdateColor();
+    }
+
+    void UpdateColor(){
+        Material material = GetComponent<MeshRenderer>().sharedMaterial;
+        if (material == null){
+            material = new Material(Shader.Find("Standard"));
+        }
+        material.color = this._color;
+        GetComponent<MeshRenderer>().material = material;
     }
 
     void MakeArrowMesh(){
@@ -45,9 +58,17 @@ public class Arrow2D : MonoBehaviour
         triangles.Add(1);
         triangles.Add(3);
 
+        triangles.Add(3);
+        triangles.Add(1);
+        triangles.Add(0);
+
         triangles.Add(0);
         triangles.Add(3);
         triangles.Add(2);
+
+        triangles.Add(2);
+        triangles.Add(3);
+        triangles.Add(0);
 
         Vector3 tipOrigin = _stemLength*Vector3.right;
         float tipHalfWidth = _pointWidth/2;
@@ -60,11 +81,19 @@ public class Arrow2D : MonoBehaviour
         triangles.Add(6);
         triangles.Add(5);
 
+        triangles.Add(5);
+        triangles.Add(6);
+        triangles.Add(4);
+
+        if (mesh == null){
+            mesh = new Mesh(); 
+        }
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
     }
 
     void OnValidate(){
         this.MakeArrowMesh();
+        this.UpdateColor();
     }
 }
